cee3fedd77fbebf8bd33430e2bb4fdf80a6eac61
Dinis Silva <aluno23031@ipt.pt>
2022-03-29 17:19:09 +0100

    Implementação da App

---
+++ Vets/Vets/Models/Donos.cs
@@ -0,0 +1,33 @@
+﻿namespace Vets.Models{
+    public class Donos
+    {
+        public Donos(){
+            ListaAnimais = new HashSet<Animais>();
+        }
+
+        /// <summary>
+        /// PK para a tabela dos Donos
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Nome o Dono do animal
+        /// </summary>
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// Número do NIF
+        /// </summary>
+        public string NIF { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Sexo { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public ICollection<Animais> ListaAnimais { get; set; }
+    }
+}
